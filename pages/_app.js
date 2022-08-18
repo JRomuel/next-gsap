a6f2e65b13aa211dd1648748432221ac19c47794
Romuel Webdev <johnromuelmendoza@yahoo.com>
2022-08-18 09:34:48 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
+import '../styles/components/app.scss'
+import { useEffect } from "react";
+import gsap from "gsap";
+
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    gsap.to("body", 0, { css: { visibility: 'visible'} });  
+  })
+  
   return <Component {...pageProps} />
 }
 
---
+++ pages/about-us.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from '../styles/components/about.module.scss'
+
+
+const About_us = () => {
+  return (
+    <div>
+        <p className="primary">About Us</p>
+        <p className={styles.primary_1}>About Us 2</p>
+    </div>
+  )
+}
+
+export default About_us
